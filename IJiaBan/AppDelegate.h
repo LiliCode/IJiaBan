57d7eb42e2c5e9545ecb81bef6b35716f41c6ef8
lili <lili2011050508@163.com>
2016-05-13 10:25:12 +0800

    Initial Commit

---
+++ IJiaBan/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  IJiaBan
+//
+//  Created by 圈圈科技 on 16/5/13.
+//  Copyright © 2016年 圈圈科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ IJiaBan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IJiaBan
+//
+//  Created by 圈圈科技 on 16/5/13.
+//  Copyright © 2016年 圈圈科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
